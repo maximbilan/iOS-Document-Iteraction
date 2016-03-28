ced0526e88c1b9f89ccb58a3159a40eb83afeb1f
Maxim Bilan <maximb.mail@gmail.com>
2016-03-28 18:55:42 +0300

    Added project template

---
+++ DocumentInteraction/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DocumentInteraction
+//
+//  Created by Maxim on 3/28/16.
+//  Copyright © 2016 Maxim Bilan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
